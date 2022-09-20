f78a07a90d4dd4e38d1870951b63d47577beeb14
Juan Carlos Santos <juasanto@student.42madrid.com>
2022-09-20 16:19:17 +0200

    Solucionado problema con los espacios

--- sources/main.c
+++ sources/main.c
@@ -54,7 +54,7 @@ char *join(const char* s1, const char* s2)
 {
     char* result = malloc(strlen(s1) + strlen(s2) + 1);
 
-    if (result) // thanks @pmg
+    if (result)
     {
         strcpy(result, s1);
         strcat(result, s2);
